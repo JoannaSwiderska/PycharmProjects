d1cbc0cee52979a8bfa065e3f133e35ed0c3f8e3
Joanna <j.swiderska83@gmail.com>
2018-02-06 14:21:26 +0100

    task with if statement - shorten version of code

--- day3/task.py
+++ day3/task.py
@@ -3,16 +3,21 @@ digit = input('Podaj liczbe: ')
 # zmiana stringu na integer
 digit = int(digit)
 
-digit_mod_2 = digit % 2
+#digit_mod_2 = digit % 2
+#
+# if digit_mod_2 == 0:
+#     is_even = 1
+# else:
+#     is_even = 0
+#
+#
+# if is_even:
+#     print('liczba jest parzysta')
+# else:
+#     print('liczba nie jest parzysta')
 
-if digit_mod_2 == 0:
-    is_even = 1
-else:
-    is_even = 0
 
-
-if is_even:
+if not digit % 2:
     print('liczba jest parzysta')
 else:
     print('liczba nie jest parzysta')
-
