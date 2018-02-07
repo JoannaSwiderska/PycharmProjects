5beb24dd9fd8bca87716801d54537df50a2e6456
Joanna <j.swiderska83@gmail.com>
2018-02-07 18:20:05 +0100

    training while loop

---
+++ day4/loops.py
@@ -0,0 +1,14 @@
+# while True:
+#     print('Hello!')
+
+counter = 0
+value = input('Podaj liczbe: ')
+value = int(value)
+
+while counter < value:
+    print ('Hello!')
+    counter += 1
+print('Bye')
+
+
+
