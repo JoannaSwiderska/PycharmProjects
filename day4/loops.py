6da235cdeda4cc55c756538273d960ea13615398
Joanna <j.swiderska83@gmail.com>
2018-02-07 18:26:46 +0100

    training while loop

--- day4/loops.py
+++ day4/loops.py
@@ -5,9 +5,24 @@ counter = 0
 value = input('Podaj liczbe: ')
 value = int(value)
 
+# while counter < value:
+#     print ('Hello!')
+#     counter += 1
+# # ten warunek jest juz poza petla
+# print('Bye')
+
+
+
+# @TODO: wywietl counter tylko jesli jest niearzysty
+
 while counter < value:
-    print ('Hello!')
-    counter += 1
+    if counter % 2:
+        print(counter)
+        counter += 1
+    else:
+        counter += 1
+        continue
+
 # ten warunek jest juz poza petla
 print('Bye')
 
